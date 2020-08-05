22d55168d90320f2119e6da2744b4a530094a306
Siddharth Surapaneni <60629151+ssiddharth2@users.noreply.github.com>
2020-08-05 19:31:52 -0400

    Updated Red EDA with Boxplots

--- Red_EDA.R
+++ Red_EDA.R
@@ -6,26 +6,170 @@ library(MASS)
 library(RColorBrewer)
 library(ggplot2)
 library(nnet)
+library(tidyverse)
 
 red<-read.csv("wineQualityReds.csv", header=TRUE)
 white<-read.csv("wineQualityWhites.csv", header=TRUE)
 attach(red)
 
+for (row in 1:nrow(red)) {
+  if (red[row, "quality"] > 6) {
+    red[row, "qualR"] <- 1
+  } else {
+    red[row, "qualR"] <- 0
+  }
+}
+
+red$qualR <- as.factor(red$qualR)
+levels(red$qualR) <- c("low", "high") 
+contrasts(red$qualR)
 ### Exploratory Data Analysis
 
+### Alcohol
+
 boxplot(alcohol~quality) # looks positive linear
+
+ggplot(red, aes(x=quality, y=alcohol, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Alcohol") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=alcohol, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Alcohol") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+
+### Volatile Acidity
+
 boxplot(volatile.acidity~quality) # possibly negative linear
 
-boxplot(sulphates~quality) # fairly horizontal
-boxplot(density~quality) # fairly horizontal
-boxplot(residual.sugar~quality) # fairly horizontal
+ggplot(red, aes(x=quality, y=volatile.acidity, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Volatile Acidity") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
 
-boxplot(pH~quality) # seems almost constant
-boxplot(total.sulfur.dioxide~quality) # seems almost constant
-boxplot(free.sulfur.dioxide~quality) # seems almost constant
-boxplot(chlorides~quality) # seems almost constant
-boxplot(citric.acid~quality)  # seems almost constant
+ggplot(red, aes(x=qualR, y=volatile.acidity, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Volatile Acidity") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+### Sulphates
+boxplot(red$sulphates~red$quality) # fairly horizontal
+
+ggplot(red, aes(x=quality, y=sulphates, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Sulphates") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=sulphates, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Sulphates") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+### Density
+boxplot(density~quality) # fairly horizontal
+
+ggplot(red, aes(x=quality, y=density, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Density") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=density, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Density") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+### Citric Acid
+boxplot(red$citric.acid~red$quality)  # fairly horizontal
+
+ggplot(red, aes(x=quality, y=citric.acid, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Citric Acid") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=citric.acid, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Citric Acid") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+### Fixed Acidity
 boxplot(fixed.acidity~quality) # seems almost constant
+
+ggplot(red, aes(x=quality, y=fixed.acidity, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Fixed Acidity") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=fixed.acidity, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Fixed Acidity") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ ### Residual Sugar
+boxplot(red$residual.sugar~red$quality) # fairly horizontal
+
+ggplot(red, aes(x=quality, y=residual.sugar, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Residual Sugar") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=residual.sugar, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Residual Sugar") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+### pH
+boxplot(pH~quality) # seems almost constant
+
+ggplot(red, aes(x=quality, y=pH, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by pH") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=pH, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by pH") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+### Chlorides
+boxplot(chlorides~quality)
+
+ggplot(red, aes(x=quality, y=chlorides, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Chlorides") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=chlorides, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Chlorides") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+### Total Sulfur Dioxide
+boxplot(total.sulfur.dioxide~quality) # seems almost constant
+
+ggplot(red, aes(x=quality, y=total.sulfur.dioxide, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Total Sulfur Dioxide") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+ggplot(red, aes(x=qualR, y=total.sulfur.dioxide, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Total Sulfur Dioxide") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+### Free Sulfur Dioxide
+boxplot(free.sulfur.dioxide~quality) # seems almost constant
+
+ggplot(red, aes(x=quality, y=free.sulfur.dioxide, group=quality)) +  
+  ggtitle("High and Low Quality \n Red Wines by Free Sulfur Dioxide") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
+### Line Plots
+ggplot(red, aes(x=qualR, y=free.sulfur.dioxide, fill=qualR)) + 
+  ggtitle("High and Low Quality \n Red Wines by Free Sulfur Dioxide") +
+  geom_boxplot(outlier.colour="red", outlier.shape=8) +
+  scale_fill_brewer(palette="Dark2")
+
 
 ggplot(aes(x=alcohol, y = quality), data = red) + 
   geom_point(aes(colour = quality), alpha = 0.5, size = 2, position = 'jitter')  + 
@@ -57,6 +201,15 @@ ggplot(aes(x=pH, y = quality), data = red) +
   ggtitle("Quality vs pH")+
   scale_colour_gradient(low = "blue", high = "orange")
 
+ggplot(aes(x=citric.acid, y = quality), data = red) + 
+  geom_point(aes(colour = quality), alpha = 0.5, size = 2, position = 'jitter')  + 
+  ggtitle("Quality vs citric acid")+
+  scale_colour_gradient(low = "blue", high = "orange")
+
+
+
+
+
 # Simple Linear Regression of Quality by Alcohol
 SLR_alcohol <- lm(quality~alcohol)
 SLR_alcohol
@@ -76,6 +229,10 @@ abline(SLR_volatile.acidity, col='cyan')
 # residual plot
 plot(SLR_volatile.acidity$fitted.values,SLR_volatile.acidity$residuals, main="Residuals: Quality by Volatile Acidity", xlab='Fitted Values', ylab='Residuals')
 abline(h=0,col="red")
+
+
+
+
 
 SLR_residual.sugar <- lm(quality~residual.sugar)
 SLR_residual.sugar
